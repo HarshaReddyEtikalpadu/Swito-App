17f9c7a53b8cc0e764dca00b622a5a590d6b8a0a
Harsha Etikalapadu <harshareddyhvr630@example.com>
2024-03-10 13:09:14 -0500

    changed containers as grid and applied directives

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FF6363', // Define the custom color
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883'
+        }
+      },
+      fontFamily: {
+        body: ['Nunito']
+      }
+    },
   },
+  variants: {},
   plugins: [],
 }
-
